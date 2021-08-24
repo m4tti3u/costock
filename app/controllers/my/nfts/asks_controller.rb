727322129a6b52cc07acc013c752a22d9f2319dd
luc laurent <laurentluc7@gmail.com>
2021-08-24 15:40:45 +0200

    config controller

---
+++ app/controllers/my/nfts/asks_controller.rb
@@ -0,0 +1,2 @@
+class My::Nfts::AsksController < ApplicationController
+end
---
+++ test/controllers/my/nfts/bids_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class My::Nfts::BidsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
