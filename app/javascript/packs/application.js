5b081bd476187f3abb2f3371aba4192986c2a77a
Matthieu Assie <matthieu.assie@gmail.com>
2021-08-31 15:47:36 +0200

    portfolio

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,9 +29,11 @@ import "chartkick/chart.js";
 // Internal imports, e.g:
 import { initSelect2 } from '../components/init_select2';
 import { changeNavbarOnScroll } from '../components/change_navbar';
+import { switchBidAsk } from '../components/switch_portfolio';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   initSelect2();
   changeNavbarOnScroll();
+  switchBidAsk();
 });
